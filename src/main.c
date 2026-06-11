59d1060450132fb394f4be014b60cd53aac4b5b5
One-GoodKnight <giniaux.alexandre@gmail.com>
2026-06-11 22:47:57 +0200

    feat: make boids teleport from border to border instead of changing velocity

--- src/main.c
+++ src/main.c
@@ -477,7 +477,7 @@ int main(void)
 	create_debug_buffer(&a);
 	a.mesh = load_mesh_from_gltf_file("assets/models/cone.glb");
 	upload_mesh(&a);
-	upload_boids(&a, 1000000);
+	upload_boids(&a, 10000);
 	upload_scene(&a);
 	create_spatial_hash_buffers(&a);
 	create_bindless_descriptors(&a);
@@ -537,6 +537,8 @@ int main(void)
 			dt *= 20;
 		else if (keyboard_state[SDL_SCANCODE_H])
 			dt *= 100;
+		else if (keyboard_state[SDL_SCANCODE_J])
+			dt *= 500;
 
 		SDL_WindowFlags wf = SDL_GetWindowFlags(a.window);
 		if (wf & SDL_WINDOW_MINIMIZED) { SDL_Delay(16); continue; }
